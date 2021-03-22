b8e3b1d2844e125692d236fdf05c980197b3a15d
Hildweig <Hildweig@gmail.com>
2021-03-22 14:56:14 +0100

    not secure at all

--- pmatel/settings.py
+++ pmatel/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.contrib.messages import constants as messages
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'pjr6kq2zn&y&g-i-7q!g5n)^yz$*d124i8+55-4*d5ej7p=-9*'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE')
-SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE')
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -94,9 +93,9 @@ AUTHENTICATION_BACKENDS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': 'pmatel_db',
+        'USER':'postgres',
+        'PASSWORD': 'miyou0209',
         
     }
 }
